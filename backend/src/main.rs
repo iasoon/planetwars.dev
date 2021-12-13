8b4440f7236b0972c1a804eea4c8305b958ad03c
Ilion Beyst <ilion.beyst@gmail.com>
2021-12-13 15:43:47 +0100

    setup basic rocket+diesel app

---
+++ backend/src/main.rs
@@ -0,0 +1,15 @@
+#![feature(proc_macro_hygiene, decl_macro)]
+
+#[macro_use]
+extern crate rocket;
+#[macro_use]
+extern crate rocket_contrib;
+
+#[get("/")]
+fn index() -> &'static str {
+    "Hello, world!"
+}
+
+fn main() {
+    rocket::ignite().mount("/", routes![index]).launch();
+}
