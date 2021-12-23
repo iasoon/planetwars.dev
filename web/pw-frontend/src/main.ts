d0af8d3bbff506d4f4caf03750c3b85c69c4f168
Ilion Beyst <ilion.beyst@gmail.com>
2021-12-23 19:25:56 +0100

    use vite-plugin-wasm-pack

--- web/pw-frontend/src/main.ts
+++ web/pw-frontend/src/main.ts
@@ -1,9 +1,7 @@
 import App from './App.svelte'
 import init_wasm_module from "planetwars-rs";
 
-const wasm_url = new URL("../planetwars-rs/pkg/planetwars_rs_bg.wasm", import.meta.url)
-
-init_wasm_module(wasm_url).then(() => {
+init_wasm_module().then(() => {
   const app = new App({
     target: document.getElementById('app')
   })
--- web/pw-frontend/vite.config.js
+++ web/pw-frontend/vite.config.js
@@ -1,11 +1,13 @@
 import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
 import { viteCommonjs } from '@originjs/vite-plugin-commonjs'
+import wasmPack from 'vite-plugin-wasm-pack';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     svelte(),
+    wasmPack(["./planetwars-rs"]),
     viteCommonjs({
       transformMixedEsModules: true,
     }),
