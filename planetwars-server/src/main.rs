3edf5d60f54bfd0cd2c818e5fb1ca133e324325d
Ilion Beyst <ilion.beyst@gmail.com>
2021-12-30 11:45:59 +0100

    rename to planetwars-server

--- planetwars-server/src/main.rs
+++ planetwars-server/src/main.rs
@@ -1,11 +1,11 @@
 use std::net::SocketAddr;
 
-extern crate mozaic4_backend;
+extern crate planetwars_server;
 extern crate tokio;
 
 #[tokio::main]
 async fn main() {
-    let app = mozaic4_backend::app().await;
+    let app = planetwars_server::app().await;
 
     let addr = SocketAddr::from(([127, 0, 0, 1], 9000));
 
